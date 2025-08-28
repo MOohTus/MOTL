115599a0a2afe57cce7d8bf086d718d54204cb51
MOohTus <caco630@hotmail.fr>
2025-08-28 16:05:17 +0200

    Add test folder

---
+++ MOTL/tests/testthat/test-TL_VI_functions.R
@@ -0,0 +1,91 @@
+test_that("TCGATargetDataPrefiltering", {
+  print("to do")
+})
+
+test_that("TCGATargetDataPreparation", {
+  print("to do")
+})
+
+test_that("mRNA_addVersion", {
+  print("to do")
+})
+
+test_that("GeoMeans_Lrn_init", {
+  print("to do")
+})
+
+test_that("preprocessCountsData", {
+  print("to do")
+})
+
+test_that("TargetDataPreparation", {
+  print("to do")
+})
+
+test_that("initTransferLearningParamaters", {
+  print("to do")
+})
+
+test_that("Tau_init", {
+  print("to do")
+})
+
+test_that("TauLn_calculation", {
+  print("to do")
+})
+
+test_that("WSq_calculation", {
+  print("to do")
+})
+
+test_that("W0_calculation", {
+  print("to do")
+})
+
+test_that("intercepts_calculation", {
+  print("to do")
+})
+
+test_that("Zeta_calculation", {
+  print("to do")
+})
+
+test_that("Tau_calculation", {
+  print("to do")
+})
+
+test_that("YGauss_calculation", {
+  print("to do")
+})
+
+test_that("ZVar_calculation", {
+  print("to do")
+})
+
+test_that("ZMu_calculation", {
+  print("to do")
+})
+
+test_that("ELBO_calculation", {
+  print("to do")
+})
+
+test_that("E_ZE_W_update", {
+  print("to do")
+})
+
+test_that("E_Z_SqE_W_Sq_update", {
+  print("to do")
+})
+
+test_that("E_ZWSq_update", {
+  print("to do")
+})
+
+test_that("VarExplFun", {
+  print("to do")
+})
+
+test_that("transferLearning_function", {
+  print("to do")
+})
