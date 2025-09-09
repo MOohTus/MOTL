e492901897f3474e37543a786dfaf0eec9a59bc8
MOohTus <caco630@hotmail.fr>
2025-09-09 14:31:01 +0200

    Unit test for Zeta_calculation

--- MOTL/tests/testthat/test-TL_VI_functions.R
+++ MOTL/tests/testthat/test-TL_VI_functions.R
@@ -272,9 +272,26 @@ test_that("intercepts_calculation", {
   print("to do")
 })
 
-test_that("Zeta_calculation", {
-  print("to do")
-    # zeta <- Zeta_calculation(view = "mRNA", likelihoods, E_ZWSq = ) 
+test_that("Zeta_calculation_", {
+  likelihoods <- list("mRNA" = "gaussian", "miRNA" = "bernoulli")
+  E_ZE_W <- list("mRNA" = E_ZE_W_update(view = "mRNA", ZMu_0, ZMu, Fctrzn_Lrn_W0, Fctrzn_Lrn_W))
+  E_Z_SqE_W_Sq <- list("mRNA" = E_Z_SqE_W_Sq_update(view = "mRNA", ZMu_0, ZMu, Fctrzn_Lrn_W0, Fctrzn_Lrn_W))
+  E_ZSqE_WSq <- list("mRNA" = E_ZSqE_WSq_update(view = "mRNA", ZMu_0, ZMuSq, Fctrzn_Lrn_W0, Fctrzn_Lrn_WSq))
+  E_ZWSq <- list("mRNA" = E_ZWSq_update(view = "mRNA", E_ZE_W, ZMuSq, E_Z_SqE_W_Sq, E_ZSqE_WSq))
+  E_ZWSq$miRNA <- E_ZWSq$mRNA
+  E_ZE_W$miRNA <- E_ZE_W$mRNA
+  zeta <- Zeta_calculation(view = "mRNA", likelihoods, E_ZWSq, E_ZE_W)
+  expect_equal(zeta, E_ZE_W$mRNA)
+  zeta <- Zeta_calculation(view = "miRNA", likelihoods, E_ZWSq, E_ZE_W)
+  expect_equal(zeta, sqrt(E_ZWSq$miRNA))
+})
+
+test_that("Zeta_calculation_others", {
+  E_ZE_W <- list("" = E_ZE_W_update(view = "mRNA", ZMu_0, ZMu, Fctrzn_Lrn_W0, Fctrzn_Lrn_W))
+  E_Z_SqE_W_Sq <- list("mRNA" = E_Z_SqE_W_Sq_update(view = "mRNA", ZMu_0, ZMu, Fctrzn_Lrn_W0, Fctrzn_Lrn_W))
+  E_ZSqE_WSq <- list("mRNA" = E_ZSqE_WSq_update(view = "mRNA", ZMu_0, ZMuSq, Fctrzn_Lrn_W0, Fctrzn_Lrn_WSq))
+  E_ZWSq <- E_ZWSq_update(view = "mRNA", E_ZE_W, ZMuSq, E_Z_SqE_W_Sq, E_ZSqE_WSq)
+  zeta <- Zeta_calculation(view = "mRNA", likelihoods, E_ZWSq, E_ZE_W) 
 })
 
 test_that("Tau_calculation", {
