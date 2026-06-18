19168eeaa9dee199822350c0b01a8505f74a59c3
MOohTus <caco630@hotmail.fr>
2026-06-18 11:22:14 +0200

    Update file paths in test

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -2,7 +2,7 @@
 ##
 
 ## DIRECTORIES FOR TEST FILES
-Lrn_ModelDir <- base::system.file("tests/testthat/fixtures/", package = "MOTL")
+Lrn_ModelDir <- base::system.file("fixtures/", package = "MOTL")
 
 ## LEARNING DATASET METADATA
 ## LEARNING DATASET FACTORIZATION MODEL
